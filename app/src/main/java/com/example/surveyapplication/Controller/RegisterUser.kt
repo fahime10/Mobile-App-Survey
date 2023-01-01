87f74a778470a5523a6cc513544c351e896e86bf
fahim <p2595607@my365.dmu.ac.uk>
2023-01-01 02:37:02 +0000

    Updated RegisterUser class to request user to insert at least 8 characters as username

--- app/src/main/java/com/example/surveyapplication/Controller/RegisterUser.kt
+++ app/src/main/java/com/example/surveyapplication/Controller/RegisterUser.kt
@@ -21,31 +21,47 @@ class RegisterUser : AppCompatActivity() {
         val loginName = findViewById<EditText>(R.id.studentUsername).text.toString()
         val password = findViewById<EditText>(R.id.studentPassword).text.toString()
 
-        if (password.length >= 8) {
-            if (loginName.startsWith("p")) {
+        if (loginName.length == 8) {
+            if (password.length >= 8) {
+                if (loginName.startsWith("p")) {
 
-                val newStudent = Student(-1, loginName, password)
-                val database = DatabaseHelper(this)
-                val result = database.addStudent(newStudent)
+                    val newStudent = Student(-1, loginName, password)
+                    val database = DatabaseHelper(this)
 
-                when (result) {
-                    1 -> {
-                        Toast.makeText(this, "You have been registered!", Toast.LENGTH_LONG).show()
-                        val returnMain = Intent(this, MainActivity::class.java)
-                        startActivity(returnMain)
+                    when (database.addStudent(newStudent)) {
+                        1 -> {
+                            Toast.makeText(
+                                this, "You have been registered!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            val returnMain = Intent(this, MainActivity::class.java)
+                            startActivity(returnMain)
+                        }
+                        -1 -> Toast.makeText(
+                            this, "Error on creating account...",
+                            Toast.LENGTH_LONG).show()
+                        -2 -> Toast.makeText(
+                            this, "Error! Cannot open database...",
+                            Toast.LENGTH_LONG).show()
+                        -3 -> Toast.makeText(
+                            this, "Username already exists!",
+                            Toast.LENGTH_LONG).show()
                     }
-                    -1 -> Toast.makeText(this, "Error on creating account...", Toast.LENGTH_LONG).show()
-                    -2-> Toast.makeText(this, "Error! Cannot open database...", Toast.LENGTH_LONG).show()
-                    -3 -> Toast.makeText(this, "Username already exists!", Toast.LENGTH_LONG).show()
+
+                } else {
+                    Toast.makeText(
+                        this, "Login name should start with 'p'!",
+                        Toast.LENGTH_LONG).show()
                 }
 
             } else {
-                Toast.makeText(this,"Login name should start with 'p'!", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this, "Password should be 8 characters!",
+                    Toast.LENGTH_LONG).show()
             }
-
         } else {
-            Toast.makeText(this,"Password should be 8 characters!", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Username should be 8 characters!",
+                Toast.LENGTH_LONG).show()
         }
-
     }
 }
